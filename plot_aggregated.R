2906dbe09abc8588c996b177499b160ba6e55234
Matteo Abis <gmatteo..abis@gmail.com>
2018-05-07 19:21:37 +0200

    fix plot

--- plot_aggregated.R
+++ plot_aggregated.R
@@ -1,21 +1,8 @@
 #!/usr/bin/env Rscript
 
 library(data.table)
-library(jsonlite)
 library(argparse)
 library(ggplot2)
-
-theme_set(theme_bw(base_size=12) + theme(
-    legend.key.size=unit(1, 'lines'),
-    text=element_text(face='plain', family='CM Roman'),
-    legend.title=element_text(face='plain'),
-    axis.line=element_line(color='black'), axis.title.y=element_text(vjust=0.1),
-    axis.title.x=element_text(vjust=0.1),
-    panel.grid.major = element_blank(),
-    panel.grid.minor = element_blank(),
-    legend.key = element_blank(),
-    panel.border = element_blank()
-))
 
 parser <- ArgumentParser(description='plot aggregated')
 parser$add_argument('aggregated', nargs=1)
@@ -37,12 +24,14 @@ plot = ggplot(aggregated) +
     labs(
          x="transmission",
          y="R",
-         colour="Material")
+         colour="Material") +
+     scale_x_continuous(expand=c(0,0)) +
+     scale_y_continuous(expand=c(0,0))
 
 X11(width=14, height=10)
 print(plot)
 ##warnings()
-width = 7
+width = 14
 factor = 0.618
 height = width * factor
 ggsave(args$output, plot, width=width, height=height, dpi=300)
